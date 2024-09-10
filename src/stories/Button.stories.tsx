eb9cfc934eb1eec58d0569be9d59899318926108
Germanvw <germanvazquezw99@gmail.com>
2024-09-10 15:03:17 -0300

    Button

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Button } from "@/components/ui/button";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/ui/Button",
+  component: Button,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: ["default", "outline", "ghost", "link"],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["sm", "md", "lg"],
+    },
+    onClick: { action: "clicked" },
+  },
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Button> = {
+  args: {
+    variant: "default",
+    size: "default",
+    children: "Click Me",
+    onClick: action("default click"),
+    disabled: false,
+    className: "",
+  },
+};
+
+export const Outline: StoryObj<typeof Button> = {
+  args: {
+    variant: "outline",
+    size: "default",
+    children: "Outline Button",
+    onClick: action("default click"),
+    disabled: false,
+    className: "",
+  },
+};
+
+export const Ghost: StoryObj<typeof Button> = {
+  args: {
+    variant: "ghost",
+    size: "default",
+    children: "Ghost Button",
+    onClick: action("default click"),
+    disabled: false,
+    className: "",
+  },
+};
+
+export const Link: StoryObj<typeof Button> = {
+  args: {
+    variant: "link",
+    size: "default",
+    children: "Link Button",
+    onClick: action("default click"),
+    disabled: false,
+    className: "",
+  },
+};
