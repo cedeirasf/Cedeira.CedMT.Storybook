3559dba774e15ac431143ee15cbd2cc9f1c4b147
Germanvw <german.vazquez@cedeira.com.ar>
2024-09-27 13:10:14 -0300

    Update Button.stories.tsx

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -34,7 +34,7 @@ export default meta;
 export const Default: StoryObj<typeof Button> = {
   args: {
     variant: "default",
-    size: "lg",
+    size: "sm",
     children: "Outline Button",
     onClick: action("default click"),
     disabled: false,
