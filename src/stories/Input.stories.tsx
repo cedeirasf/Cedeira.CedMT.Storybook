b1bc48d8ba7f3d606657fcdb43f6129a33da31f9
fcolavini <facundo.colavini@cedeira.com.ar>
2024-12-10 11:53:25 -0300

    feat: mejorar variantes de input y agregar compatibilidad con temas claros y oscuros

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from "@storybook/react"
-import { Input } from "../components/ui/Input"
 import React from "react"
+import type { Meta, StoryObj } from "@storybook/react"
+import { Input } from '../components/ui/input';
 
 const meta: Meta<typeof Input> = {
   title: "Components/ui/Input",
